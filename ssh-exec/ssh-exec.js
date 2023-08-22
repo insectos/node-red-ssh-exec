228e9cc175f6e659886670197137b05e0910d050
Stephan H. Wissel <stw@linux.com>
2023-08-21 18:37:19 -0700

    isolate credentials, populate utils

--- ssh-exec/ssh-exec.js
+++ ssh-exec/ssh-exec.js
@@ -1,85 +1,13 @@
+/* (C) 2023, stwissel, Apache-2.0 license */
 'use strict';
 
 const utils = require('./utils');
-
-const debug = true;
-const minTimeout = 500;
-const maxTimeout = 1000 * 60 * 20; // 20 minutes
-
-// Create the client with event listeners
-const createClient = (node, state) => {
-  let host = state.ssh_config.host;
-  let Client = require('ssh2').Client;
-  let conn = new Client();
-
-  conn
-    .on('error', (e) => {
-      utils.errorStatus(node, host, e);
-      conn.end();
-      state.connection = undefined;
-      node.stream = undefined;
-    })
-    .on('ready', () => {
-      utils.connectStatus(node, host);
-      state.connected = true;
-
-      conn.shell(function (err, stream) {
-        if (err) {
-          node.error('ERRSHELL', { errMsg: err });
-          conn.end();
-          state.connection = undefined;
-          return;
-        }
-
-        console.log('Shell opened');
-
-        node.stream = stream;
-
-        stream
-          .on('close', function () {
-            utils.closeStatus(node, host, 'close');
-            state.connection = undefined;
-            node.stream = undefined;
-          })
-          .on('error', function (error) {
-            utils.errorStatus(node, host, error);
-          })
-          .on('data', function (data) {
-            utils.connectStatus(node, host);
-            node.send({ host: host, payload: data });
-          })
-          .stderr.on('data', function (data) {
-            utils.connectStatus(node, host);
-            node.send({ host: host, payload: data, stderr: true });
-          });
-
-        while (state.queue.length > 0) {
-          let d = state.queue.shift();
-          stream.write(d);
-        }
-      });
-
-      // TODO: data
-    })
-    .on('close', function () {
-      utils.closeStatus(node, host, 'close');
-      state.connection = undefined;
-      node.stream = undefined;
-    })
-    .on('end', function () {
-      utils.closeStatus(node, host, 'end');
-      state.connection = undefined;
-      node.stream = undefined;
-    })
-    .connect(state.ssh_config);
-
-  return conn;
-};
 
 module.exports = function (RED) {
   /* ssh connection */
   function SshExec(config) {
     RED.nodes.createNode(this, config);
+    let password = this.credentials.pass;
     let node = this;
 
     let state = {
@@ -90,44 +18,7 @@ module.exports = function (RED) {
     };
 
     node.on('input', function (msg) {
-      const data = msg.payload;
-
-      // Check if host was overwritten and needs reconnect
-      const msghost = msg.sshhost;
-      if (!state.ssh_config || (msghost && msghost != state.lastHost)) {
-        state.ssh_config = utils.createConnectCfg(config, msg);
-        if (state.connection) {
-          state.connection.end();
-          state.connection = undefined;
-        }
-      }
-
-      // (Re)build the client
-      if (!state.connection) {
-        console.log('Create new client');
-        state.queue.push(data);
-        state.connection = createClient(node, state);
-        return;
-      }
-
-      if (!node.stream) {
-        console.log('Queuing up data');
-        state.queue.push(data);
-        return;
-      }
-
-      try {
-        if (node.stream.writable) {
-          node.stream.write(data);
-        } else {
-          console.log('Stream not currently writable. Try again.');
-          node.error('Stream not currently writable. Try again.', {
-            errmsg: 'Stream not currently writable. Try again.'
-          });
-        }
-      } catch (e) {
-        node.error('Error writing to stream', { errmsg: e });
-      }
+      utils.processMessage(node, config, state, msg, password);
     });
 
     node.on('close', function (done) {
@@ -142,6 +33,9 @@ module.exports = function (RED) {
   }
 
   RED.nodes.registerType('ssh-exec', SshExec, {
+    credentials: {
+      pass: { type: 'password' }
+    },
     settings: {
       sshExecHosts: {
         value: utils.extractHosts(),
--- ssh-exec/utils.js
+++ ssh-exec/utils.js
@@ -1,10 +1,18 @@
+/* (C) 2023, stwissel, Apache-2.0 license */
 'use strict';
 
 const fs = require('fs');
 const SSHConfig = require('ssh-config');
+const ssh2 = require('ssh2');
 
 let sshCfg;
 
+/**
+ * Reads synchronous the content of a text file
+ * into a string
+ * @param {string} source File name to read
+ * @returns {string}
+ */
 const getStringFromFile = (source) => {
   const data = fs.readFileSync(source, {
     encoding: 'utf8',
@@ -40,7 +48,7 @@ const extractHosts = () => {
   return result;
 };
 
-const createConnectCfg = (config, msg) => {
+const createConnectCfg = (config, msg, password) => {
   let host = msg.sshhost ? msg.sshhost : config.sshconfig;
 
   if (host == '-manual-') {
@@ -50,7 +58,7 @@ const createConnectCfg = (config, msg) => {
       port: config.port,
       keepaliveInterval: 5000,
       username: config.username,
-      password: config.pass
+      password: password
     };
   }
 
@@ -65,10 +73,121 @@ const createConnectCfg = (config, msg) => {
     username: hostCfg.User,
     privateKey: fs.readFileSync(fileName)
   };
-  if (config.pass) {
-    ssh_config.password = config.pass;
+  if (password) {
+    ssh_config.passphrase = password;
   }
   return ssh_config;
+};
+
+// Create the client with event listeners
+const createClient = (node, state) => {
+  let host = state.ssh_config.host;
+  let Client = ssh2.Client;
+  let conn = new Client();
+
+  conn
+    .on('error', (e) => {
+      errorStatus(node, host, e);
+      conn.end();
+      state.connection = undefined;
+      node.stream = undefined;
+    })
+    .on('ready', () => {
+      connectStatus(node, host);
+      state.connected = true;
+
+      conn.shell(function (err, stream) {
+        if (err) {
+          node.error('ERRSHELL', { errMsg: err });
+          conn.end();
+          state.connection = undefined;
+          return;
+        }
+
+        console.log('Shell opened');
+
+        node.stream = stream;
+
+        stream
+          .on('close', function () {
+            closeStatus(node, host, 'close');
+            state.connection = undefined;
+            node.stream = undefined;
+          })
+          .on('error', function (error) {
+            errorStatus(node, host, error);
+          })
+          .on('data', function (data) {
+            connectStatus(node, host);
+            node.send({ host: host, payload: data });
+          })
+          .stderr.on('data', function (data) {
+            connectStatus(node, host);
+            node.send({ host: host, payload: data, stderr: true });
+          });
+
+        while (state.queue.length > 0) {
+          let d = state.queue.shift();
+          stream.write(d);
+        }
+      });
+
+      // TODO: data
+    })
+    .on('close', function () {
+      closeStatus(node, host, 'close');
+      state.connection = undefined;
+      node.stream = undefined;
+    })
+    .on('end', function () {
+      closeStatus(node, host, 'end');
+      state.connection = undefined;
+      node.stream = undefined;
+    })
+    .connect(state.ssh_config);
+
+  return conn;
+};
+
+const processMessage = (node, config, state, msg, password) => {
+  const data = msg.payload;
+
+  // Check if host was overwritten and needs reconnect
+  const msghost = msg.sshhost;
+  if (!state.ssh_config || (msghost && msghost != state.lastHost)) {
+    state.ssh_config = createConnectCfg(config, msg, password);
+    if (state.connection) {
+      state.connection.end();
+      state.connection = undefined;
+    }
+  }
+
+  // (Re)build the client
+  if (!state.connection) {
+    console.log('Create new client');
+    state.queue.push(data);
+    state.connection = createClient(node, state);
+    return;
+  }
+
+  if (!node.stream) {
+    console.log('Queuing up data');
+    state.queue.push(data);
+    return;
+  }
+
+  try {
+    if (node.stream.writable) {
+      node.stream.write(data);
+    } else {
+      console.log('Stream not currently writable. Try again.');
+      node.error('Stream not currently writable. Try again.', {
+        errmsg: 'Stream not currently writable. Try again.'
+      });
+    }
+  } catch (e) {
+    node.error('Error writing to stream', { errmsg: e });
+  }
 };
 
 const errorStatus = (node, host, e) => {
@@ -97,5 +216,7 @@ module.exports = {
   createConnectCfg: createConnectCfg,
   errorStatus: errorStatus,
   connectStatus: connectStatus,
-  closeStatus: closeStatus
+  closeStatus: closeStatus,
+  createClient: createClient,
+  processMessage: processMessage
 };
